3f514d7599364510001c888e17f5112298e594d8
Nature-Freak-Germany <72653633+Nature-Freak-Germany@users.noreply.github.com>
2021-02-11 15:58:53 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,14 +3,14 @@ exports.run = async(client, message) => {
         embed: {
             title: 'Help',
             description: `
-            +play <songName> - Play a song from youtube
-            +pause - pause music
-            +resume - resume music
-            +np - Get now playing song's info
-            +skip - Skip to next song
-            +stop - Stop playing music
-            +volume <value> - adjust volume of the music
-            +queue - to see the full song queue
+            *play <songName> - Play a song from youtube
+            *pause - pause music
+            *resume - resume music
+            *np - Get now playing song's info
+            *skip - Skip to next song
+            *stop - Stop playing music
+            *volume <value> - adjust volume of the music
+            *queue - to see the full song queue
             `,
             color: 'GREEN'
         }
